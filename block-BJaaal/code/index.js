d28366d3e7f9c7f13cad9bfaeb4cdbebfbb6976e
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-03-01 23:00:47 +0530

    modification

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -16,6 +16,7 @@ let lastName = "Stark";
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
 alert("I am" +" "+ userName+" "+"my bio is"+" "+bio);
+alert("I\'m"+" "+firstName+" "+lastName+" of winterfell")
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
@@ -32,7 +33,7 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 alert(`The total amount is ${amount}
-the tax amount is `+ [(amount * taxPercent) / 100])
+the tax amount is  ${(amount * taxPercent) / 100}`);
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
@@ -40,4 +41,4 @@ the tax amount is `+ [(amount * taxPercent) / 100])
   - Above message should be in two lines
 */
 alert("The total amount is"+" "+ amount+
-"\n"+"the tax amount is "+ [(amount * taxPercent) / 100])
+"\n the tax amount is "+ [(amount * taxPercent) / 100])
